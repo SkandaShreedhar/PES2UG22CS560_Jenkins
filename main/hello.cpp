5da2b141c9a8590ab00c280232b2ef3b1d10b014
Skanda Shreedhar <115539247+SkandaShreedhar@users.noreply.github.com>
2025-03-18 18:59:15 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -8,5 +8,6 @@ int main(void) {
   cout << "I have successfully built and run this  \n";
   cout << "SRN: PES2UG22CS560\n";
   cout << "Name: Skanda M S\n";
+  cout << "Section: 'H'\n";
   return 0;
 }
